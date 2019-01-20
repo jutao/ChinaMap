8eee3f17145f9c328ee7c860c407c3fccbab5332
jutao <573723112@qq.com>
2019-01-20 16:25:51 +0800

    svg test

--- app/src/main/java/chinamap/jt/msi/chinamap/MainActivity.java
+++ app/src/main/java/chinamap/jt/msi/chinamap/MainActivity.java
@@ -1,7 +1,10 @@
 package chinamap.jt.msi.chinamap;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView imageView=findViewById(R.id.iv);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AnimatedVectorDrawable animatedVectorDrawable= (AnimatedVectorDrawable) imageView.getDrawable();
+                animatedVectorDrawable.start();
+            }
+        });
     }
 }
